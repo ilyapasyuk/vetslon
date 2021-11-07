fc286be119c069351f8f4e44809df7b527be4a4a
ilyapasyuk <pasyuk.com@gmail.com>
2021-11-07 14:01:04 +0300

    added send mail

--- pages/api/sendMail.ts
+++ pages/api/sendMail.ts
@@ -1,32 +1,32 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const userData = JSON.parse(req.body);
+  const userData = JSON.parse(req.body)
 
-  const nodemailer = require("nodemailer");
+  const nodemailer = require('nodemailer')
 
   const transporter = nodemailer.createTransport({
     port: 587,
-    host: 'smtp.gmail.com',
+    host: process.env.MAIL_CLIENT,
     auth: {
-      user: 'intellect.mo.tech@gmail.com',
-      pass: '_admintech1989',
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASSWD,
     },
     secure: false,
-  });
+  })
 
   const mailData = {
-    from: 'noreply.intellect.mo@gmail.com',
-    to: 'intellect.mo.tech@gmail.com',
+    from: process.env.SEND_FROM,
+    to: process.env.SEND_TO,
     subject: `Message From ${userData.email}`,
-    text: "Заявка на занятие",
-    html: `<div>dateIn: ${userData.firstName}</div><p>dateOut: ${userData.lastName}</p><p>fullName: ${userData.email}</p><p>autoNumber: ${userData.phoneNumber}</p>`,
-  };
+    text: 'Заявка на парковку',
+    html: `<div>dateIn: ${userData.dateIn}</div><p>dateOut: ${userData.dateOut}</p><p>fullName: ${userData.fullName}</p><p>autoNumber: ${userData.autoNumber}</p><p>phone: ${userData.phone}</p><p>email: ${userData.email}</p>`,
+  }
 
   transporter.sendMail(mailData, function (err: any, info: any) {
-    if (err) console.log(err);
-    else console.log(info);
-  });
+    if (err) console.log(err)
+    else console.log(info)
+  })
 
-  res.send(200);
-};
+  res.send(200)
+}
